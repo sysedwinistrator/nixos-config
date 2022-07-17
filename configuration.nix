47a56f4010c8f3a96fe368ba60555ae79f4433f1
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2022-07-17 15:58:50 +0200

    some packages

--- configuration.nix
+++ configuration.nix
@@ -70,10 +70,14 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    nmap
+    tmux
+    htop
+    git
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
