6cad3e072a25efc9a55d7eb33f8367b952061a8a
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2022-07-17 15:53:00 +0200

    root ssh access

--- configuration.nix
+++ configuration.nix
@@ -88,6 +88,11 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  # root ssh access
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM+ResginDO3aOjCVc+5JIbvnxaw58LEwRhTSsv6JHJ4 edwin@dhyana"
+  ];
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
