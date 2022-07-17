823e7468c9bfef2747f883b57e7629cd68a95939
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2022-07-17 15:55:04 +0200

    set hostname

--- configuration.nix
+++ configuration.nix
@@ -15,7 +15,7 @@
   # Enables the generation of /boot/extlinux/extlinux.conf
   boot.loader.generic-extlinux-compatible.enable = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "plato"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
