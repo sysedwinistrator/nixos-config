44e131113fbf07afb5dba328171a86e03b32128f
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2022-07-24 17:30:17 +0200

    add aristotle

---
+++ configuration/aristotle.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ...}:
+
+{
+  imports = [
+    ../common.nix
+    ../hardware-configuration/plato.nix
+  ];
+
+  config = {
+    host.name = "aristotle";
+    host.hostId = "8e6e09c0";
+    host.rancher = {
+      enable = true;
+      roles = ["worker"];
+      sshKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM+ResginDO3aOjCVc+5JIbvnxaw58LEwRhTSsv6JHJ4 edwin@dhyana"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII32nGkOM9WitJaDsiboxbQAVjpemC5uUotOFlFqiYhY rancher"
+      ];
+    };
+  };
+}
