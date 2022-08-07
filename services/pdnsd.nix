45c89a3a60c477ae8e6d89bf8c2d5ffd8adcee50
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2022-08-07 20:54:32 +0200

    pdnsd

--- services/pdnsd.nix
+++ services/pdnsd.nix
@@ -6,7 +6,7 @@ with lib;
   config = lib.mkIf config.host.pdnsd.enable {
     services.pdnsd = {
       enable = true;
-      extraConfig = ''
+      serverConfig = ''
         launch=gsqlite3;
         gsqlite3-database=/var/lib/powerdns/pdns.sqlite3;
         webserver=true;
