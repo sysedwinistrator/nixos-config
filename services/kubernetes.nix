7ca5d1c31067ad7c3caa09e07a4ac197b2908636
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2022-09-17 18:58:27 +0200

    style: make kubernetes.nix more readable

--- services/kubernetes.nix
+++ services/kubernetes.nix
@@ -3,16 +3,18 @@
 let
   master = builtins.elemAt (builtins.filter (x: builtins.elem "master" x.kubernetes_roles) config.lab.all_hosts) 0;
   api = "https://${master.ip}:${builtins.toString config.services.kubernetes.apiserver.securePort}";
+  is_master = builtins.elem "master" config.lab.current_host.kubernetes_roles;
+  is_node = builtins.elem "node" config.lab.current_host.kubernetes_roles;
 in
 {
-  config = lib.mkIf (config.lab.current_host.kubernetes_roles != []) {
+  config = lib.mkIf (is_master || is_node) {
     fileSystems = lib.mkIf config.host.zfs {
       "/var/lib/containerd" = {
         device = "${config.host.zfsDataSet}/containerd";
         fsType = "zfs";
         options = [ "zfsutils" ];
       };
-      "/var/lib/etcd" = lib.mkIf (builtins.elem "master" config.lab.current_host.kubernetes_roles) {
+      "/var/lib/etcd" = lib.mkIf is_master {
         device = "${config.host.zfsDataSet}/etcd";
         fsType = "zfs";
         options = [ "zfsutils" ];
@@ -31,12 +33,12 @@ in
       apiserverAddress = api;
 
       # master configuration
-      apiserver = lib.mkIf (builtins.elem "master" config.lab.current_host.kubernetes_roles) {
+      apiserver = lib.mkIf is_master {
         advertiseAddress = master.ip;
       };
 
       # node configuration
-      kubelet.kubeconfig.server = lib.mkIf (builtins.elem "node" config.lab.current_host.kubernetes_roles) api;
+      kubelet.kubeconfig.server = lib.mkIf is_node api;
     };
   };
 }
